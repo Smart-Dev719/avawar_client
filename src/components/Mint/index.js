46b137fb7d0662b831b8436b047704b67e6f1b45
Smart-Dev719 <blockchaindev719@gmail.com>
2022-05-07 18:51:45 -0400

    minted marketplace view

--- src/components/Mint/index.js
+++ src/components/Mint/index.js
@@ -119,7 +119,7 @@ const Mint = (props) => {
                 </div>
                 <div className="ModalDescription d-flex flex-column align-items-center">
                   <span className="ModalTitle">Congratulations!</span>
-                  <a href="https://testnets.nftrade.com/assets/fuji/0xd151621eedbefb10704acebe1c841550cb8ac83e" className="ModalText" target="_blank"
+                  <a href="https://nftrade.com/assets/avalanche/0x5f241e003ba8cb0450dfef2d6fbb508e318a088a" className="ModalText" target="_blank"
                     rel="noreferrer">You have successfully minted your AvaWar NFT !</a>
                 </div>
               </div>
